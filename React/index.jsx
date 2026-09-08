19d39affc5417565c6f096e7c30bb2dd64e070bc
Ahmad karmi <ahmad2karmi@gmail.com>
2026-09-08 15:12:42 +0300

    Delete React directory

--- React/index.jsx
+++
@@ -1,7 +0,0 @@
-import ReactDOM from "react-dom/client";
-
-import App from "./APP.jsx";
-import "../style.css";
-
-const entryPoint = document.getElementById("root");
-ReactDOM.createRoot(entryPoint).render(<App></App>);
